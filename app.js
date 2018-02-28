37032c308a8c823adff867831e3b721013a91114
lautar0K <laukpo@gmail.com>
2018-02-28 00:11:04 -0300

    Added timestamp response

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
-const html =
-"<!DOCTYPE html><title>title</title></head><body><ol><li>Success</li><li>Success</li></ol></body></html>";
 
 app.get("/", function(req, res) {
-  res.end(html);
+  if(Number.isInteger(req) == true) {
+    let json = {
+      "unix": req,
+      "natural": new Date(req * 1000)
+    };
+    res.end(json);
+  }
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
