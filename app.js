b83b6e6ca5a7fbee396c448d4578481ae7c649a6
lautar0K <laukpo@gmail.com>
2018-02-27 22:58:05 -0300

    Set port

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const app = express();
 app.get("/", function(req, res) {
   res.end("Hola");
 });
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port ", this.address().port, app.settings.env)
+});
