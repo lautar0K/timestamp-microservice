644291638bff0d2a5ef6b0f08b3403be4e2dd74a
lautar0K <laukpo@gmail.com>
2018-02-28 01:15:59 -0300

    Log

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/:id", function(req, res) {
+app.get("/:", function(req, res) {
   let id = Number(req.params.id);
   let json;
 
-  console.log("ID is", typeof id);
-
   if(Number.isInteger(id) == true) {
-    console.log("Is UNIX");
     json = {
       "unix": id,
       "natural": new Date(id * 1000)
     };
   }
-  console.log("JSON: ", json);
   res.json(json);
 });
 app.listen(process.env.PORT || 3000, function() {
