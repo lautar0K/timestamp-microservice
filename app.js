6dbe68b064b18357aa4412adbfcf04f68f3b5018
lautar0K <laukpo@gmail.com>
2018-02-28 18:48:23 -0300

    Fixed if statement typo

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ app.get("/:id", function(req, res) {
     console.log("//// Case 1 ////");
     json.unix = id;
     json.natural = moment.unix(id).format("MMMM D, YYYY");
-  } else if(Number(id) == NaN && new Date(id) != "Invalid Date") {
-    console.log("//// Case 2 ////");
-    json.unix = moment(id).format("X");
-    json.natural = moment(id).format("MMMM D, YYYY");
+  } else if(Number(id) == NaN) {
+     if(new Date(id) != "Invalid Date") {
+       console.log("//// Case 2 ////");
+       json.unix = moment(id).format("X");
+       json.natural = moment(id).format("MMMM D, YYYY");
+     }
   } else {
     console.log("//// Case 3 ////");
     json.unix = null;
