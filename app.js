f325ccedcab821d5bbb99088f33852988c527e25
lautar0K <laukpo@gmail.com>
2018-02-28 18:44:05 -0300

    Fixed if statement typo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/:id", function(req, res) {
 
   console.log(id);
 
-  if(typeof Number(id) == "Number") {
+  if(typeof Number(id) == "number") {
     console.log("//// Case 1 ////");
     json.unix = id;
     json.natural = moment.unix(id).format("MMMM D, YYYY");
