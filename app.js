81f3354a454b46dbb0474516873bcda8615c706f
lautar0K <laukpo@gmail.com>
2018-02-28 01:01:04 -0300

    Log

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/:id", function(req, res) {
-  console.log(req.params);
+  console.log(req.params.id);
 
-  if(Number.isInteger(req) == true) {
+  /* if(Number.isInteger(req) == true) {
     let json = {
       "unix": req,
       "natural": new Date(req * 1000)
     };
     res.end(json);
-  }
+  }*/
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
