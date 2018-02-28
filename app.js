0ac183640d8d576e2f34bd832e37ce565a671f06
lautar0K <laukpo@gmail.com>
2018-02-27 23:58:26 -0300

    Tried html

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const html =
+"<!DOCTYPE html><title>title</title></head><body><ol><li>Success</li><li>Success</li></ol></body></html>";
 
 app.get("/", function(req, res) {
-  res.end("Hola");
+  res.end(html);
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
